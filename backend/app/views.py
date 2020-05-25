9d1550fc0f61fa72aa70517edb035b8d826998cd
saurabh <saurabh.prasad197@gmail.com>
2020-05-25 17:46:26 -0400

    backend frontend integration

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,31 +1,55 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask import jsonify
+import json
 from flask import request
+from app.entities.blog import Blog
 
-blogs = {}
+blogs = []
+blog1 = Blog("title 1", "desc1", 1)
+blog2 = Blog("title 2", "desc2 from backend", 2)
+blogs.insert(blog1.blog_id,blog1)
+blogs.insert(blog2.blog_id,blog2)
+# blogs[2] = blog2
 
 def register_view(app, api):
-    api.add_resource(Blog, '/test/<string:book_id>')
-    api.add_resource(Todo3, '/hello', '/hello2')
+    # api.add_resource(BlogService, '/blogs/<string:book_id>')
+    api.add_resource(BlogListApi, '/blogs')
+    api.add_resource(BlogApi, '/blogs/<int:id>')
 
-class Blog(Resource):
-    def get(self,book_id):
-        return {book_id:blogs[book_id]}
+class BlogListApi(Resource):
+    def get(self):
+        return blogs
 
-    def put(self, book_id):
-        blogs[book_id] = request.form['data']
-        return {book_id: blogs[book_id]}
+    # def post(self, book_id):
+    #     blogs[book_id] = request.form['data']
+    #     return {book_id: blogs[book_id]}
+
+
+class BlogApi(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json', required = True,
+            help = 'No title provided')
+        self.reqparse.add_argument('description', type = str, location = 'json', required = True,
+            help = 'No description provided')
+        self.reqparse.add_argument('blod_id', type = int, location = 'json')
+        super(BlogApi, self).__init__()
+
+    def get(self, id):
+        # Set the response code to 201 and return custom headers
+        return blogs[id]
+
+    # def put(self, book_id):
+    #     task = filter(lambda t: t['id'] == id, tasks)
+    #     if len(task) == 0:
+    #         abort(404)
+    #     task = task[0]
+    #     args = self.reqparse.parse_args()
+    #     for k, v in args.iteritems():
+    #         if v != None:
+    #             task[k] = v
+    #     return jsonify({'task': make_public_task(task)})
 
     def delete(self, book_id):
         del blogs[book_id]
         return '', 204
-
-class Blogs(Resource):
-    def get(self):
-        return blogs
-
-
-
-class Todo3(Resource):
-    def get(self):
-        # Set the response code to 201 and return custom headers
-        return {'task': 'Hello world'}, 201, {'Etag': 'some-opaque-string'}
--- frontend-ng/webapp/src/app/blogs/blog-list.component.ts
+++ frontend-ng/webapp/src/app/blogs/blog-list.component.ts
@@ -19,6 +19,7 @@ export class BlogListComponent implements OnInit, OnDestroy {
     this.blogsListSubs = this.blogsApi
       .getBlogs()
       .subscribe(res => {
+          console.log(res);
           this.blogsList = res;
         },
         console.error
